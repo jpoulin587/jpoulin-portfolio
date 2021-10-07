9657739afdea7881ee91084e1be9d0c643eb777a
jpoulin587 <jpoulin587a@gmail.com>
2021-10-07 13:17:10 -0500

    initial commit, set up file structure

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+
+export default function App() {
+  return (
+    <main> 
+
+      <p>Start here</p>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+
+    </main>
+
+    );
+}
