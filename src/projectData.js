30a88bb145ed864966c2e8360c2cd87bf744de0a
jpoulin587 <jpoulin587a@gmail.com>
2021-10-07 17:38:04 -0500

    update project list data

---
+++ src/projectData.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    title: "CyberBarberShop1",
+    subtitle: "Full stack with mySql",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-placeholder.png",
+    githubLink: "https://github.com/jpoulin587/CyberBarbershop",
+    deployedLink: "https://barber-shop55.herokuapp.com/"
+
+  },
+  {
+    title: "CyberBarberShop2",
+    subtitle: "Full stack with mySql",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-placeholder.png",
+    githubLink: "https://github.com/jpoulin587/CyberBarbershop",
+    deployedLink: "https://barber-shop55.herokuapp.com/"
+
+  },{
+    title: "CyberBarberShop3",
+    subtitle: "Full stack with mySql",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-placeholder.png",
+    githubLink: "https://github.com/jpoulin587/CyberBarbershop",
+    deployedLink: "https://barber-shop55.herokuapp.com/"
+
+  },{
+    title: "CyberBarberShop4",
+    subtitle: "Full stack with mySql",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-placeholder.png",
+    githubLink: "https://github.com/jpoulin587/CyberBarbershop",
+    deployedLink: "https://barber-shop55.herokuapp.com/"
+
+  },{
+    title: "CyberBarberShop5",
+    subtitle: "Full stack with mySql",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-placeholder.png",
+    githubLink: "https://github.com/jpoulin587/CyberBarbershop",
+    deployedLink: "https://barber-shop55.herokuapp.com/"
+
+  },{
+    title: "React Reserve",
+    subtitle: "Full stack with mySql",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    githubLink: "https://github.com/jpoulin587/CyberBarbershop",
+    deployedLink: "https://barber-shop55.herokuapp.com/"
+
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
