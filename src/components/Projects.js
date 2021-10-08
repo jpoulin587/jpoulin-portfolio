59a0422a369494eb9a60940c210c5f341fc9352b
jpoulin587 <jpoulin587a@gmail.com>
2021-10-08 16:33:27 -0500

    skills page complete

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,6 @@ import { projects } from "../projectData";
 const Projects = () => {
     return (
         <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-            <p>from the Projects component</p> 
             <div className="container px-5 py-10 mx-auto text-center lg:px-40">
                 <div className="flex flex-col w-full mb-20">
                     <h1 className="sm:text-4x1 text-3x1 font-medium title-font mb-4 text-white">
@@ -17,17 +16,17 @@ const Projects = () => {
                 <div className="grid grid-flow-row grid-cols-2 -m-2">
                     {projects.map((project) => (
                         <a
-                        href={project.githubLink}
-                        /*href={project.deployedLink}*/
+                        /*href={project.githubLink}*/
+                        href={project.deployedLink}
                         key={project.image}
-                        className="col-span-2 sm:col-span-1 p-4">
+                        className="col-span-2 sm:col-span-1 p-8">
                         <div className="flex relative">
                             <img
                                 alt="gallery"
                                 className="absolute inset-0 w-auto h-auto object-cover object-center"
                                 src={project.image}
                             />
-                            <div className="px-8 py-10 relative z-10 w-full border-gray-800 bg-gray-900 opacity-25 hover:opacity-100">
+                            <div className="px-8 py-10 relative z-10 w-full border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                                 <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                                     {project.subtitle}
                                 </h2>
