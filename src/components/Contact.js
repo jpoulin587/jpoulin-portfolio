1d1b9fd72ddf15d6d59fb00310182f2193c843cc
jpoulin587 <jpoulin587a@gmail.com>
2021-10-08 19:47:18 -0500

    contact page complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,21 +3,101 @@ import React from 'react'
 const Contact = () => {
     return (
         <section id="contact" className="relative">
-            <p>from the contact component</p>  
-            <div className="container px-5 py-10 mx-auto flex sm:flex-">
-                <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative"> 
-
-                </div>
-
+        <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            
+            <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+              <div className="lg:w-1/2 px-6">
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                  MY HOMETOWN
+                </h2>
+                <p className="mt-1">
+                  Ft. Lauderdale, FL
+                  <br />
+                  <br />
+                </p>
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                  MY GITHUB
+                </h2>
+                <a href="https://github.com/jpoulin587"  className="text-indigo-400 mt-1">
+                github.com/jpoulin587
+                </a>
+              </div>
+              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                  EMAIL
+                </h2>
+                <p className="text-indigo-400 leading-relaxed">
+                  jpoulin587a@gmail.com
+                </p>
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                  PHONE
+                </h2>
+                <p className="leading-relaxed">123-456-7890</p>
+                <br />
+                
+                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                  LinkedIn
+                </h2>
+                <a href="https://www.linkedin.com/in/jim-poulin/" className="text-indigo-400 leading-relaxed">
+                www.linkedin.com/in/jim-poulin/
+                </a>
+                
+              </div>
             </div>
-
-
-
-
-
-
-
-        </section>
+          </div>
+          <form
+            netlify
+            name="contact"
+            className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+              Let's Connect
+            </h2>
+            <p className="leading-relaxed mb-5">
+           Put your details in here and I will get back to you.
+            </p>
+            <div className="relative mb-4">
+              <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="message"
+                className="leading-7 text-sm text-gray-400">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Submit
+            </button>
+          </form>
+        </div>
+      </section>
     )
 }
 
